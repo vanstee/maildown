9c9128596a9561e0b96e3ad877a40dc5d3ac59af
vanstee <patrick@vanstee.me>
2013-07-15 18:55:52 -0400

    Throw up a fixture like route of notes

--- app.rb
+++ app.rb
@@ -2,3 +2,17 @@ require 'sinatra'
 
 get '/' do
 end
+
+get '/notes' do
+  %(
+    {
+      "notes": [
+        {
+          "from": "vanstee@bignerdranch.com",
+          "subject": "This s a test",
+          "body": "Testing the markdown stuffs. <b>Bold</b> <i>Fancy</i>"
+        }
+      ]
+    }
+  )
+end
